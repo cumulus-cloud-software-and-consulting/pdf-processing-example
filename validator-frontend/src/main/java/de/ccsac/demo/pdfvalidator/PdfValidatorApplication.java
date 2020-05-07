c6d42a366e35d6113fb87baf05a38f968c5d0872
Rainer Bieniek <Rainer.Bieniek@cumulus-cloud-consulting.de>
2020-05-07 18:25:27 +0200

    add frontend and backend sub-projects

---
+++ validator-frontend/src/main/java/de/ccsac/demo/pdfvalidator/PdfValidatorApplication.java
@@ -0,0 +1,13 @@
+package de.ccsac.demo.pdfvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfValidatorApplication.class, args);
+	}
+
+}
---
+++ validator-frontend/src/test/java/de/ccsac/demo/pdfvalidator/PdfValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ccsac.demo.pdfvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
